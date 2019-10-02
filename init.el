9d5a766808177452b95dfd0f0de023366f014ba9
icyzeroice <ziv3@outlook.com>
2019-10-02 16:35:20 +0800

    :arrow_up: update

--- init.el
+++ init.el
@@ -526,7 +526,8 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js-indent-level 2))
+ '(js-indent-level 2)
+ '(js2-strict-missing-semi-warning nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
