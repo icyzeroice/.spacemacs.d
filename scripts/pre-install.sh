4435ab9a10c3ceeed94c2e0ff435a2eca1eb8f65
icyzeroice <ziv3@outlook.com>
2019-10-15 00:28:28 +0800

    add org notes

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,4 +1,13 @@
 # DO NOT use the shell in emacs!!!
+
+# Org LaTeX
+sudo apt install \
+     texlive-latex-base \
+     texlive-fonts-recommended \
+     texlive-fonts-extra \
+     texlive-latex-extra
+
+# JavaScript Layer
 npm install -g \
     import-js \
     eslint \
