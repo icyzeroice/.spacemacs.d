07c515123c2efc538c16894b8f0d7e3892ff9f86
icyzeroice <ziv3@outlook.com>
2019-10-15 22:04:15 +0800

    :arrow_up: add tex installing in mac os

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,11 +1,16 @@
 # DO NOT use the shell in emacs!!!
 
 # Org LaTeX
+## Ubtuntu
 sudo apt install \
      texlive-latex-base \
      texlive-fonts-recommended \
      texlive-fonts-extra \
      texlive-latex-extra
+## Mac OS (select one of below)
+# brew cask install mactex
+# brew cask install basictex
+
 
 # JavaScript Layer
 npm install -g \
