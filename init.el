74c2237b0dddbd1d7c8dae88541c50cfdeb2bd2b
icyzeroice <ziv3@outlook.com>
2020-11-30 14:49:14 +0800

    update cnfonts config

--- init.el
+++ init.el
@@ -582,22 +582,37 @@ before packages are loaded."
   ;; Oh, MyGod! 中文对齐了吗Example
   ;; ---------------------------------------------------------
   ;; https://github.com/tumashu/cnfonts
-  ;; (cnfonts-enable)
-  ;; (cnfonts-set-spacemacs-fallback-fonts)
-  (set-face-attribute
-   'default nil
-   :font (font-spec :name "-outline-Consolas-bold-italic-normal-mono-*-*-*-*-c-*-iso10646-1"
-                    :weight 'normal
-                    :slant 'normal
-                    :size 10.0))
-  (dolist (charset '(kana han symbol cjk-misc bopomofo))
-    (set-fontset-font
-     (frame-parameter nil 'font)
-     charset
-     (font-spec :name "-outline-微软雅黑-normal-normal-normal-sans-*-*-*-*-p-*-iso10646-1"
-                :weight 'normal
-                :slant 'normal
-                :size 12.0)))
+  (pcase system-type
+    ('windows-nt (progn (set-face-attribute
+                          'default nil
+                          :font (font-spec :name "-outline-Consolas-bold-italic-normal-mono-*-*-*-*-c-*-iso10646-1"
+                                          :weight 'normal
+                                          :slant 'normal
+                                          :size 10.0))
+                        (dolist (charset '(kana han symbol cjk-misc bopomofo))
+                          (set-fontset-font
+                            (frame-parameter nil 'font)
+                            charset
+                            (font-spec :name "-outline-微软雅黑-normal-normal-normal-sans-*-*-*-*-p-*-iso10646-1"
+                                      :weight 'normal
+                                      :slant 'normal
+                                      :size 12.0)))))
+    ('darwin (progn (set-face-attribute
+                      'default nil
+                      :font (font-spec :name "-*-Monaco-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
+                                      :weight 'normal
+                                      :slant 'normal
+                                      :size 12.5))
+                    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+                      (set-fontset-font
+                        (frame-parameter nil 'font)
+                        charset
+                        (font-spec :name "-*-Hiragino Sans GB-normal-normal-normal-*-*-*-*-*-p-0-iso10646-1"
+                                  :weight 'normal
+                                  :slant 'normal
+                                  :size 15.0)))))
+    (code (progn (cnfonts-enable)
+                 (cnfonts-set-spacemacs-fallback-fonts))))
 
   )
 
--- scripts/elisp-example/example.el
+++ scripts/elisp-example/example.el
@@ -3,10 +3,17 @@
 ;; (print (member "STHeitiSC" (font-family-list)))
 ;; (print (member "Sarasa Gothic SC" (font-family-list)))
 ;; (print (member "更纱黑体 UI SC" (font-family-list)))
-(message (member "Microsoft Yahei Mono" (font-family-list)))
+;; (message (member "Microsoft Yahei Mono" (font-family-list)))
 ;; (print (font-family-list))
-
 ;; (dolist (name (font-family-list))
 ;;   (print name))
+
+(pcase system-type
+  ('darwin (progn (print 1)
+                  (print 2)))
+  ('windows-nt (progn (print 3)
+                      (print 4)))
+  (code (progn (print 5)
+               (print system-type))))
 
 
